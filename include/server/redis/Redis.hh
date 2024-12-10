de4cf9e3adcf9e523838e67d2cd6209c8747b226
xygxgn <hcxygxgn@163.com>
2024-12-10 06:55:18 -0800

    first commit

---
+++ include/server/redis/Redis.hh
@@ -0,0 +1,26 @@
+#ifndef __REDIS_HH
+#define __REDIS_HH
+
+#include <hiredis/hiredis.h>
+#include <functional>
+#include <string>
+
+class Redis
+{
+public:
+    Redis();
+    ~Redis();
+
+    bool connect(std::string ip = "127.0.0.1", int port = 6379);
+    bool publish(int channel, const std::string &msg);
+    bool subscribe(int channel);
+    bool ubsubscribe(int channel);
+    void observeChannelMessage();
+    void setNotifyHandler(std::function<void(int, std::string)> func);
+private:
+    redisContext *pubContext_;
+    redisContext *subContext_;
+    std::function<void(int, std::string)> notifyMsgHandler_;
+};
+
+#endif
---
+++ src/server/redis/Redis.cc
@@ -0,0 +1,112 @@
+#include "server/redis/Redis.hh"
+
+#include <iostream>
+#include <thread>
+
+Redis::Redis() : pubContext_(nullptr), subContext_(nullptr)
+{
+}
+
+Redis::~Redis()
+{
+    if (pubContext_ != nullptr)
+        ::redisFree(pubContext_);
+    if (subContext_ != nullptr)
+        ::redisFree(subContext_);
+}
+
+bool Redis::connect(std::string ip, int port)
+{
+    pubContext_ = redisConnect(ip.c_str(), port);
+    if (pubContext_ == nullptr)
+    {
+        std::cerr << "connect redis failed!" << std::endl;
+        return false;
+    }
+
+    subContext_ = redisConnect(ip.c_str(), port);
+    if (subContext_ == nullptr)
+    {
+        std::cerr << "connect redis failed!" << std::endl;
+        return false;
+    }
+
+    std::thread t(&Redis::observeChannelMessage, this);
+    t.detach();
+
+    return true;
+}
+
+bool Redis::publish(int channel, const std::string &msg)
+{
+    redisReply *reply = (redisReply*)redisCommand(pubContext_, "PUBLISH %d %s", channel, msg.c_str());
+    if (reply == nullptr)
+    {
+        std::cerr << "publish command failed!" << std::endl;
+        return false;
+    }
+
+    freeReplyObject(reply);
+    return true;
+}
+
+bool Redis::subscribe(int channel)
+{
+    if (redisAppendCommand(subContext_, "SUBSCRIBE %d", channel) == REDIS_ERR)
+    {
+        std::cerr << "subscribe command failed!" << std::endl;
+        return false;
+    }
+
+    int done = 0;
+    while (!done)
+    {
+        if (redisBufferWrite(subContext_, &done) == REDIS_ERR)
+        {
+            std::cerr << "subscribe command failed" << std::endl;
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+bool Redis::ubsubscribe(int channel)
+{
+    if (redisAppendCommand(subContext_, "UNSUBSCRIBE %d", channel) == REDIS_ERR)
+    {
+        std::cerr << "unsubscribe command failed!" << std::endl;
+        return false;
+    }
+
+    int done = 0;
+    while (!done)
+    {
+        if (redisBufferWrite(subContext_, &done) == REDIS_ERR)
+        {
+            std::cerr << "unsubscribe command failed" << std::endl;
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+void Redis::observeChannelMessage()
+{
+    redisReply *reply = nullptr;
+    while (redisGetReply(subContext_, (void**)&reply) == REDIS_OK)
+    {
+        if (reply != nullptr && reply->element[2] != nullptr && reply->element[2]->str != nullptr)
+        {
+            notifyMsgHandler_(std::atoi(reply->element[1]->str), reply->element[2]->str);
+        }
+        freeReplyObject(reply);
+    }
+    std::cerr << ">>>>>>>>>>>>> observeChannelMessage quit <<<<<<<<<<<<<" << std::endl;
+}
+
+void Redis::setNotifyHandler(std::function<void(int, std::string)> func)
+{
+    notifyMsgHandler_ = func;
+}
